9d4d1c6cba34cc148d75f686dc93f46929d17cb5
ZAF07 <zaffere07@gmail.com>
2021-02-01 13:58:23 +0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('http://localhost:3000'));
